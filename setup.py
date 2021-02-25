8375ef2231a31c2beec34c7ebca1a27aeaa69562
Amogh Desai <amoghd9@gmail.com>
2021-02-25 08:33:37 +0530

    Inital Commit ++ Cookiecutter

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='A novel, state of the art fact-checker to verify news facts.',
+    author='Amogh Desai',
+    license='MIT',
+)
