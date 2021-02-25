fbfa65e4dee02ff4ec3b3bb782cb95af7466199e
Amogh Desai <amoghd9@gmail.com>
2021-02-25 09:25:33 +0530

    Perfected pre-commit-config ++ Minor changes

--- setup.py
+++ setup.py
@@ -1,10 +1,10 @@
 from setuptools import find_packages, setup
 
 setup(
-    name='src',
+    name="src",
     packages=find_packages(),
-    version='0.1.0',
-    description='A novel, state of the art fact-checker to verify news facts.',
-    author='Amogh Desai',
-    license='MIT',
+    version="0.1.0",
+    description="A novel, state of the art fact-checker to verify news facts.",
+    author="Amogh Desai",
+    license="MIT",
 )
